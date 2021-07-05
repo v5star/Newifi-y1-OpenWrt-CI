c07d208b3df905a436e3d6604eada23d161e826e
v5star <31580191+v5star@users.noreply.github.com>
2021-07-05 13:46:00 +0800

    Update script.sh

--- newifi-y/script.sh
+++ newifi-y/script.sh
@@ -2,13 +2,13 @@
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
 # 修改菜单名称（试验）
-sed -i 's/"管理权"/"改密码"/g' feeds/luci/modules/luci-base/po/zh-cn/base.po
+#sed -i 's/"管理权"/"改密码"/g' feeds/luci/modules/luci-base/po/zh-cn/base.po
 
 # 修改默认IP为192.168.3.1
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 
 # Uncomment a feed source 取消feeds注释，以便更新
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld ' feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld ' feeds.conf.default
